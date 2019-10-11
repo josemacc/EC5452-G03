dca4151618c1ca70e92ffc30ebea26823e97ad3d
Adrian Gonzalez <adgp391997@gmail.com>
2019-10-11 15:20:55 -0400

    video4/35:47. Addiing more format

--- resources/views/categories/table.blade.php
+++ resources/views/categories/table.blade.php
@@ -1,34 +1,29 @@
-<table class="table table-responsive" id="categories-table">
-    <thead>
-        <tr>
-            <th>Name</th>
-        <th>Description</th>
-        <th>View Count</th>
-            <th colspan="3">Action</th>
-        </tr>
-    </thead>
-    <tbody> 
-    
-    <div class="card-deck">
 
+ <div class="card-deck">
     @foreach($categories as $category)
 
-    <div class="card">
+        <div class="card" style="width: 18rem;">
         <img src="..." class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h5 class="card-title">{!! $category->name !!}</h5>
+            <p class="card-text">{!! $category->description !!}</p>
         </div>
-        <div class="card-footer">
-        <small class="text-muted">Last updated 3 mins ago</small>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">{!! $category->view_count !!}</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+        <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
         </div>
-    </div>
+        </div>
+    @endforeach
+</div>
 
-        <tr>
-            <td>{!! $category->name !!}</td>
-            <td>{!! $category->description !!}</td>
-            <td>{!! $category->view_count !!}</td>
-            <td>
+
+<!-- 
+
                 {!! Form::open(['route' => ['categories.destroy', $category->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
                     <a href="{!! route('categories.show', [$category->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
@@ -36,10 +31,5 @@
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
                 {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
-
-    </div>
-    </tbody>
-</table>
+ 
+ -->
