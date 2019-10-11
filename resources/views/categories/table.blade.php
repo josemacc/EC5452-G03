699c9485e28505ce942d5110f1c720bb1d0872d6
Adrian Gonzalez <adgp391997@gmail.com>
2019-10-11 16:21:50 -0400

    video4/44:56. Limiting the quantity of characters in description section

--- resources/views/categories/table.blade.php
+++ resources/views/categories/table.blade.php
@@ -6,11 +6,11 @@
         <img src="https://image.shutterstock.com/z/stock-vector-grunge-red-sample-word-round-rubber-seal-stamp-on-white-background-1242668641.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{!! $category->name !!}</h5>
-            <p class="card-text">{!! $category->description !!}</p>
+            <p class="card-text">{{ str_limit($category->description, $limit = 150, $end = '...') }}</p>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Views: {!! $category->view_count !!}</li>
+            </ul>
         </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Views: {!! $category->view_count !!}</li>
-        </ul>
         <div class="card-footer">
         <a href="#" class="btn btn-primary">View Category</a>
         </div>
