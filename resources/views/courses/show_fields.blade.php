dc3af96da8da09f975237c55244466903686f208
miguelangel2801 <miguelangel2801@gmail.com>
2019-10-29 11:58:13 -0300

    Procesador de Pagos con sprite

--- resources/views/courses/show_fields.blade.php
+++ resources/views/courses/show_fields.blade.php
@@ -44,9 +44,57 @@
                 <span style="font-size: 18px; text-decoration:line-through">
                     ${!! $course->actual_price !!}</span>
 
-                    <div class:"col-md-12">
-                    <a href="#" class="btn btn-lg btn-success"> Buy Now ${!! $course->discount_price !!} </a> 
+                    <script src="https://js.stripe.com/v3"></script>
+   
+                  <!--   <div class:"col-md-12" >
+                    <a href="" class="btn btn-lg btn-success" id="checkout-button-sku_G1q6jDxU8kOaku"
+                      role="link"> Buy Now ${!! $course->discount_price !!} </a> 
+                    </div> -->
+                    <div class:"col-md-12" >            
+                    <button
+                      style="background-color:#4BB543;color:#FFF;padding:8px 12px;border:0;border-radius:4px;font-size:1em"
+                      id="checkout-button-sku_G1q6jDxU8kOaku"
+                      role="link"
+                    >
+                      Buy Now ${!! $course->discount_price !!}
+                    </button>
+                    
                     </div>
+                    <div id="error-message"></div>
+
+                    <script>
+                    (function() {
+                      var stripe = Stripe('pk_test_1LdVPQkda6Ewhk1LjH39CMu700WM6noH7I');
+
+                      var checkoutButton = document.getElementById('checkout-button-sku_G1q6jDxU8kOaku');
+                      checkoutButton.addEventListener('click', function () {
+                        // When the customer clicks on the button, redirect
+                        // them to Checkout.
+                        stripe.redirectToCheckout({
+                          items: [{sku: 'sku_G1q6jDxU8kOaku', quantity: 1}],
+
+                          // Do not rely on the redirect to the successUrl for fulfilling
+                          // purchases, customers may not always reach the success_url after
+                          // a successful payment.
+                          // Instead use one of the strategies described in
+                          // https://stripe.com/docs/payments/checkout/fulfillment
+                          successUrl: 'http://127.0.0.1:8000/',
+                          cancelUrl: 'http://127.0.0.1:8000/',
+                        })
+                        .then(function (result) {
+                          if (result.error) {
+                            // If `redirectToCheckout` fails due to a browser or network
+                            // error, display the localized error message to your customer.
+                            var displayError = document.getElementById('error-message');
+                            displayError.textContent = result.error.message;
+                          }
+                        });
+                      });
+                    })();
+                    </script>
+
+
+
             </div>
 
        
